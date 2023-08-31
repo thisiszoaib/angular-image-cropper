0e79f1be2518bd50acfb3774863262f595430a67
Zoaib <thisiszoaib@gmail.com>
2023-08-31 18:12:50 +0500

    Starting point for video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { ImageControlComponent } from './components/image-control/image-control.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, ImageControlComponent],
-  template: `
-    <mat-toolbar color="primary"> Angular Image Cropper </mat-toolbar>
-
-    <div class="container">
-      <app-image-control
-        [width]="200"
-        [height]="250"
-        (imageReady)="imageReady($event)"
-      />
-    </div>
-  `,
-  styles: [
-    `
-      .container {
-        padding: 24px;
-      }
-    `,
-  ],
+  imports: [CommonModule],
+  template: ``,
+  styles: [``],
 })
-export class AppComponent {
-  imageReady(blob: Blob) {
-    console.log('The image control gave back this blob', blob);
-  }
-}
+export class AppComponent {}
